e6cef44c680e7f96fee03a8cd263a0ef905ad824
Shaktyka <k-i-s-u-n-j-a@list.ru>
2021-11-25 07:40:33 +0500

    Добавляет класс MarvelService

---
+++ src/services/MarvelService.js
@@ -0,0 +1,19 @@
+class MarverService {
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  getAllCharacters = () => {
+    return this.getResource('https://gateway.marvel.com:443/v1/public/characters?apikey=4246451221b77ae47492166344c89484');
+  }
+
+}
+
+export default MarverService;
